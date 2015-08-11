a066c0ea63cd23fd508fae0bb77c84907e29344a
Danil <difeid@yandex.ru>
2015-08-12 00:52:13 +0300

    Добавлен конструктор

--- InteractiveTable/Pages/MemoryViewer.xaml.cs
+++ InteractiveTable/Pages/MemoryViewer.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Markup;
+using System.Globalization;
 
 namespace InteractiveTable.Pages
 {
@@ -20,9 +23,29 @@ namespace InteractiveTable.Pages
     /// </summary>
     public partial class MemoryViewer : Page
     {
-        public MemoryViewer()
+        public MemoryViewer(string item, int number)
         {
             InitializeComponent();
+
+            Uri pathMain = new Uri(String.Format("/MemoryViewer/{0}/main.{1}.jpg", item, number), UriKind.Relative);
+            Uri pathSub = new Uri(String.Format("/MemoryViewer/{0}/sub.{1}.jpg", item, number), UriKind.Relative);
+            try
+            {
+                mainPhoto.Source = new BitmapImage(pathMain);
+                subPhoto.Fill = new ImageBrush(new BitmapImage(pathSub));
+            }
+            catch (IOException) { }
+
+            //Опредяляем текущий язык
+            string culture = App.Language.Name;
+
+            Uri pathDisc = new Uri(String.Format("/MemoryViewer/{0}/disc.{1}.{3}.xaml", item, number, culture), UriKind.Relative);
+            try
+            {
+                FlowDocument doc = Application.LoadComponent(pathDisc) as FlowDocument;
+                documentDiscription.Document = doc;
+            }
+            catch (IOException) { }
         }
 
         private void Back_Button_Click(object sender, RoutedEventArgs e)
