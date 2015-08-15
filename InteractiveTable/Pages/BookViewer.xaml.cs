a89c16219324ffd4bb988550979f5c1323ac63cf
Danil <difeid@yandex.ru>
2015-08-16 01:38:44 +0300

    Вертикальная кнопка

--- InteractiveTable/Pages/BookViewer.xaml.cs
+++ InteractiveTable/Pages/BookViewer.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MoonPdfLib;
 
 namespace InteractiveTable.Pages
 {
@@ -23,6 +24,11 @@ namespace InteractiveTable.Pages
         public BookViewer()
         {
             InitializeComponent();
+        }
+
+        private void Content_Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
