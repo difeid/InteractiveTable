863d263c84916c2415b4d1d4c6ac5a51a98a79ed
Danil <difeid@yandex.ru>
2015-08-19 23:24:50 +0300

    Удаление using

--- InteractiveTable/App.xaml.cs
+++ InteractiveTable/App.xaml.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Globalization;
+using System.Linq;
+using System.Windows;
 
 namespace InteractiveTable
 {
