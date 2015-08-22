af6bf28cf11c9dc195a3cee98f094d0ed54ec827
Danil <difeid@yandex.ru>
2015-08-22 15:07:12 +0300

    Fix

--- InteractiveTable/Pages/BrothersSister.xaml.cs
+++ InteractiveTable/Pages/BrothersSister.xaml.cs
@@ -18,22 +18,22 @@ namespace InteractiveTable.Pages
             this.NavigationService.GoBack();
         }
 
-        private void Dmitry_Button_Click(object sender, RoutedEventArgs e)
+        private void Nikola_Button_Click(object sender, RoutedEventArgs e)
         {
             new PopupWindow("BrothersSister", 1, 0).Show();
         }
 
-        private void Mary_Button_Click(object sender, RoutedEventArgs e)
+        private void Sergey_Button_Click(object sender, RoutedEventArgs e)
         {
             new PopupWindow("BrothersSister", 1, 1).Show();
         }
 
-        private void Nikola_Button_Click(object sender, RoutedEventArgs e)
+        private void Dmitry_Button_Click(object sender, RoutedEventArgs e)
         {
             new PopupWindow("BrothersSister", 1, 2).Show();
         }
 
-        private void Sergey_Button_Click(object sender, RoutedEventArgs e)
+        private void Mary_Button_Click(object sender, RoutedEventArgs e)
         {
             new PopupWindow("BrothersSister", 1, 3).Show();
         }
