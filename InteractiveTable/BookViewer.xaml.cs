261952c77b684e7093df17384d18d5e316e1666c
Danil <difeid@yandex.ru>
2015-08-23 14:07:47 +0300

    Добавил содержание и разбивку на части

--- InteractiveTable/BookViewer.xaml.cs
+++ InteractiveTable/BookViewer.xaml.cs
@@ -1,20 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO;
-using System.Windows.Threading;
 using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace InteractiveTable
 {
@@ -24,48 +17,50 @@ namespace InteractiveTable
     public partial class BookViewer : Window
     {
         private string bookName;
+        private int partCount;
+        private int currentPart = 0;
+
         protected Point TouchStart;
         private bool AlreadySwiped;
         private string culture;
 
         private bool IsToggle;
-        DoubleAnimation da = new DoubleAnimation();
+        private DoubleAnimation da = new DoubleAnimation();
+        private bool IsLocked;
 
-        public BookViewer(string bookName)
+        private Button[] but;
+        private Line[] line;
+        private int countLines = 0;
+
+        public BookViewer(string bookName, int partCount)
         {
             InitializeComponent();
 
+            App.BookOpen = true;
             slidePanel.Width = 0;
+
             culture = App.Language.Name;
             this.bookName = bookName;
+            this.partCount = partCount;
 
+            but = new Button[partCount];
+            line = new Line[partCount];
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = new TimeSpan(0, 0, 0, 1, 0);
-            timer.Tick += (s, ar) =>
-            {
-                timer.Stop();
+            OpenContents(bookName, culture);
+            OpenBook(currentPart);
+        }
 
-                FlowDocument book = OpenBook(bookName, culture);
-                OpenContents(bookName, culture);
-                if (book == null)
-                {
-                    if (culture != "ru-RU")
-                    {
-                        book = OpenBook(bookName, "ru-RU");
-                    }
-                }
-                bookReader.Document = book;
-                pleaseWaitPopup.IsOpen = false;
-            };
-            timer.Start();
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            App.BookOpen = false;
         }
 
         private void Back_Button_Click(object sender, RoutedEventArgs e)
         {
+            pleaseWaitPopup.IsOpen = false;
             this.Close();
         }
 
@@ -77,6 +72,152 @@ namespace InteractiveTable
                 da.Duration = TimeSpan.FromMilliseconds(300);
                 slidePanel.BeginAnimation(Grid.WidthProperty, da);
                 IsToggle = true;
+            }
+        }
+
+        private void Point_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsLocked)
+            {
+                int part = Convert.ToInt32((sender as Button).Name);
+                if (part != currentPart)
+                {
+                    OpenBook(part);
+                }
+            }
+        }
+
+        private void OpenBook(int part)
+        {
+            if (part >= 0 && part < partCount)
+            {
+                IsLocked = true;
+                HighlightingButton(part, currentPart);
+                pleaseWaitPopup.IsOpen = true;
+                DispatcherTimer timer = new DispatcherTimer();
+                timer.Interval = new TimeSpan(0, 0, 0, 1, 0);
+                timer.Tick += (s, ar) =>
+                {
+                    timer.Stop();
+
+                    FlowDocument book = OpenPartBook(bookName, part, culture);
+                    OpenContents(bookName, culture);
+                    if (book == null)
+                    {
+                        if (culture != "ru-RU")
+                        {
+                            book = OpenPartBook(bookName, part, "ru-RU");
+                        }
+                    }
+                    if (book != null)
+                    {
+                        bookReader.Document = book;
+                    }
+                    else
+                    {
+                        bookReader.Document.Blocks.Clear();
+                    }
+                    currentPart = part;
+                    pleaseWaitPopup.IsOpen = false;
+                    IsLocked = false;
+                };
+                timer.Start();
+            }
+        }
+
+        private void HighlightingButton( int selectPart, int canselSelectPart)
+        {
+            if (selectPart < countLines && canselSelectPart < countLines && but[canselSelectPart] != null && but[selectPart] != null)
+            {
+                if (canselSelectPart != selectPart)
+                {
+                    but[canselSelectPart].FontWeight = FontWeights.Normal;
+                    but[canselSelectPart].IsEnabled = true;
+                }
+                but[selectPart].FontWeight = FontWeights.Bold;
+                but[selectPart].IsEnabled = false;
+            }
+        }
+
+        public FlowDocument OpenPartBook(string bookName, int part, string culture)
+        {
+            string path = String.Format("Contents/Book/{0}/book.{1}.{2}.rtf", bookName, part, culture);
+
+            FlowDocument book = null;
+
+            if (File.Exists(path))
+            {
+                book = new FlowDocument();
+                TextRange tr = new TextRange(book.ContentStart, book.ContentEnd);
+                try
+                {
+                    using (FileStream fs = File.Open(path, FileMode.Open))
+                    {
+                        tr.Load(fs, DataFormats.Rtf);
+                    }
+                }
+                catch (IOException) { }
+
+                book.ColumnWidth = 800;
+                book.PagePadding = new Thickness(80,170,80,170);
+            }
+            return book;
+        }
+
+        private bool OpenContents(string bookName, string culture)
+        {
+            string path = String.Format("Contents/Book/{0}/contents.{1}.txt", bookName, culture);
+
+            if (File.Exists(path))
+            {
+                string[] lines = File.ReadAllLines(path);
+                countLines = lines.Length;
+                int num = 0;
+
+                ResourceDictionary dict = new ResourceDictionary();
+                dict.Source = new Uri("/InteractiveTable;component/Resource/ResourceDictionaryTemplate.xaml", UriKind.RelativeOrAbsolute);
+
+                //<Line Stroke="Gray" StrokeThickness="0.5" Width="360" X1="0" Y1="0" X2="360" Y2="0"/>
+                Line l = new Line();
+                l.X1 = 0;
+                l.X2 = 360;
+                l.Y1 = 0;
+                l.Y2 = 0;
+                l.Width = 360;
+                l.StrokeThickness = 1;
+                l.Stroke = Brushes.Gray;
+                stack.Children.Add(l);
+
+                while (num < countLines)
+                {
+                    //<Button Width="360" Height="50" Margin="5" Template="{StaticResource HiddenButtonTemplate}" Tag="Лев Николаевич Толстой" FontSize="16"/>
+                    but[num] = new Button();
+                    but[num].Width = 360;
+                    but[num].Height = 50;
+                    but[num].Margin = new Thickness(5);
+                    but[num].Template = dict["HiddenButtonTemplate"] as ControlTemplate;
+                    but[num].Tag = lines[num];
+                    but[num].FontSize = 16;
+                    but[num].FocusVisualStyle = null;
+                    but[num].Name = num.ToString();
+                    but[num].Click += Point_Button_Click;
+                    stack.Children.Add(but[num]);
+
+                    line[num] = new Line();
+                    line[num].X1 = 0; line[num].X2 = 360;
+                    line[num].Y1 = 0; line[num].Y2 = 0;
+                    line[num].Width = 360;
+                    line[num].StrokeThickness = 0.5;
+                    line[num].Stroke = Brushes.Gray;
+                    stack.Children.Add(line[num]);
+
+                    num++;
+                }
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
 
@@ -113,12 +254,16 @@ namespace InteractiveTable
 
                     //Swipe Left
 
-                    if (Touch.X < (TouchStart.X - 200) && TouchStart.X >1000)
+                    if (Touch.X < (TouchStart.X - 200) && TouchStart.X > 1000)
                     {
                         //RunMyCustomCode();
                         if (bookReader.CanGoToNextPage)
                         {
                             NavigationCommands.NextPage.Execute(null, bookReader);
+                        }
+                        else
+                        {
+                            OpenBook(currentPart + 1);
                         }
                         AlreadySwiped = true;
 
@@ -132,6 +277,10 @@ namespace InteractiveTable
                         {
                             NavigationCommands.PreviousPage.Execute(null, bookReader);
                         }
+                        else
+                        {
+                            OpenBook(currentPart - 1);
+                        }
                         AlreadySwiped = true;
                     }
                 }
@@ -142,84 +291,6 @@ namespace InteractiveTable
         private void Memory_ScrollViewer_ManipulationBoundaryFeedback(object sender, ManipulationBoundaryFeedbackEventArgs e)
         {
             e.Handled = true;
-        }
-
-        public FlowDocument OpenBook(string bookName, string culture)
-        {
-            string path = String.Format("Contents/Book/{0}/book.{1}.rtf", bookName, culture);
-
-            FlowDocument book = null;
-
-            if (File.Exists(path))
-            {
-                book = new FlowDocument();
-                TextRange tr = new TextRange(book.ContentStart, book.ContentEnd);
-                try
-                {
-                    using (FileStream fs = File.Open(path, FileMode.Open))
-                    {
-                        tr.Load(fs, DataFormats.Rtf);
-                    }
-                }
-                catch (IOException) { }
-
-                book.ColumnWidth = 800;
-                book.PagePadding = new Thickness(80,170,80,170);
-            }
-            return book;
-        }
-
-        public void OpenContents(string bookName, string culture)
-        {
-            string path = String.Format("Contents/Book/{0}/contents.{1}.txt", bookName, culture);
-
-            if (File.Exists(path))
-            {
-                string[] lines = File.ReadAllLines(path);
-                int count = lines.Length;
-                int num = 0;
-
-                Button[] but = new Button[count];
-                Line[] line = new Line[count];
-
-                ResourceDictionary dict = new ResourceDictionary();
-                dict.Source = new Uri("/InteractiveTable;component/Resource/ResourceDictionaryTemplate.xaml",UriKind.RelativeOrAbsolute);
-
-                //<Line Stroke="Gray" StrokeThickness="0.5" Width="360" X1="0" Y1="0" X2="360" Y2="0"/>
-                Line l = new Line();
-                l.X1 = 0;
-                l.X2 = 360;
-                l.Y1 = 0;
-                l.Y2 = 0;
-                l.Width = 360;
-                l.StrokeThickness = 0.5;
-                l.Stroke = Brushes.Gray;
-                stack.Children.Add(l);
-
-                while (num < count)
-                {
-                    //<Button Width="360" Height="50" Margin="5" Template="{StaticResource HiddenButtonTemplate}" Tag="Лев Николаевич Толстой" FontSize="16"/>
-                    but[num] = new Button();
-                    but[num].Width = 360;
-                    but[num].Height = 50;
-                    but[num].Margin = new Thickness(5);
-                    but[num].Template = dict["HiddenButtonTemplate"] as ControlTemplate;
-                    but[num].Tag = lines[num];
-                    but[num].FontSize = 16;
-                    but[num].FocusVisualStyle = null;
-                    stack.Children.Add(but[num]);
-
-                    line[num] = new Line();
-                    line[num].X1 = 0; line[num].X2 = 360;
-                    line[num].Y1 = 0; line[num].Y2 = 0;
-                    line[num].Width = 360;
-                    line[num].StrokeThickness = 0.5;
-                    line[num].Stroke = Brushes.Gray;
-                    stack.Children.Add(line[num]);
-
-                    num++;
-                }
-            }
         }
     }
 }
