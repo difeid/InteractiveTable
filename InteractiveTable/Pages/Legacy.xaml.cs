1959c66b0eb7caf0f9c446f860cfaf4e3be4cc7e
Danil <difeid@yandex.ru>
2015-08-23 21:34:21 +0300

    Смена названий

--- InteractiveTable/Pages/Legacy.xaml.cs
+++ InteractiveTable/Pages/Legacy.xaml.cs
@@ -19,62 +19,17 @@ namespace InteractiveTable.Pages
             this.NavigationService.GoBack();
         }
 
-        public void OpenBook(string bookName, int partCount)
+        private void Book_Button_Click(object sender, RoutedEventArgs e)
         {
-            if (!App.BookOpen)
+            int bookNumber = Convert.ToInt32((sender as Button).Name.Substring(1, 1));
+            if (!App.PopupOpen)
             {
-                new BookViewer(bookName, partCount).Show();
+                try
+                {
+                    new PopupWindow("Book", 10, bookNumber).Show();
+                }
+                catch { }
             }
-        }
-
-        private void Rules_Of_Life_Button_Click(object sender, RoutedEventArgs e)
-        {
-            OpenBook("RulesOfLife", 4);
-        }
-
-        private void Trilogy_Button_Click(object sender, RoutedEventArgs e)
-        {
-            OpenBook("Trilogy", 4);
-        }
-
-        private void Diary_Button_Click(object sender, RoutedEventArgs e)
-        {
-            OpenBook("Diary", 5);
-        }
-
-        private void Memories_Button_Click(object sender, RoutedEventArgs e)
-        {
-            OpenBook("Memories", 4);
-        }
-
-        private void Morning_Button_Click(object sender, RoutedEventArgs e)
-        {
-            OpenBook("Morning", 4);
-        }
-
-        private void Cossacks_Button_Click(object sender, RoutedEventArgs e)
-        {
-            OpenBook("Cossacks", 4);
-        }
-
-        private void War_And_Peace_Button_Click(object sender, RoutedEventArgs e)
-        {
-            OpenBook("WarAndPeace", 4);
-        }
-
-        private void After_Ball_Button_Click(object sender, RoutedEventArgs e)
-        {
-            OpenBook("AfterBall", 4);
-        }
-
-        private void Yule_Night_Button_Click(object sender, RoutedEventArgs e)
-        {
-            OpenBook("YuleNight", 4);
-        }
-
-        private void Oasis_Button_Click(object sender, RoutedEventArgs e)
-        {
-            OpenBook("Oasis", 4);
         }
     }
 }
