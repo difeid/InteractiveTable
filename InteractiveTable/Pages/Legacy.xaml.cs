a528492852995df7eeba3280c3677a15dc2d674f
Danil <difeid@yandex.ru>
2015-08-23 14:08:06 +0300

    Revert name button

--- InteractiveTable/Pages/Legacy.xaml.cs
+++ InteractiveTable/Pages/Legacy.xaml.cs
@@ -22,7 +22,57 @@ namespace InteractiveTable.Pages
         private void Book_Button_Click(object sender, RoutedEventArgs e)
         {
             int bookNumber = Convert.ToInt32((sender as Button).Name.Substring(1, 1));
-            new BookViewer(bookNumber.ToString()).Show();
+            //new BookViewer(bookNumber.ToString(),).Show();
+        }
+
+        private void Rules_Of_Life_Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Trilogy_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+        }
+
+        private void Diary_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+        }
+
+        private void Memories_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+        }
+
+        private void Morning_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+        }
+
+        private void Cossacks_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+        }
+
+        private void War_And_Peace_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+        }
+
+        private void After_Ball_Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.GoBack();
+        }
+
+        private void Yule_Night_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Oasis_Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
